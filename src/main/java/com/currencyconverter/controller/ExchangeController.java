0d2fa0e03006939d96f727299cd60f5c11a61dc4
Pushkar <pushkar.r.adhikari@gmail.com>
2016-11-14 18:46:17 +0530

    Fixed Request Mapping on Build failure

--- src/main/java/com/currencyconverter/controller/ExchangeController.java
+++ src/main/java/com/currencyconverter/controller/ExchangeController.java
@@ -18,16 +18,25 @@ public class ExchangeController {
 	public String getExchangeRateByCurrency(String currency) {
 			return businessServiceObj.getExchangeRateByCurrency(currency);
 	}
-
+	
 	@RequestMapping("/value")
 	@ResponseBody
+	public String getConvertedValue(@RequestParam("currency") String currency,
+			@RequestParam("rate") double rate,
+			@RequestParam("amount") double amount)
+	{
+		if (currency.equals(null)) {
+			return getConvertedValueFromRateAndAmount(rate, amount);
+		} else{
+			return getConvertedValueFromCurrencyAndAmount(currency, amount);
+		}
+	}
+
 	public String getConvertedValueFromRateAndAmount(double rate, double amount) {
 		return businessServiceObj.getResultValueFromRateAndAmount(rate, amount);
 	}
 
-	@RequestMapping("/value")
-	@ResponseBody
-	public String getConvertedValueFromCurrencyAndAmount(@RequestParam("currency") String currency, double amount) {
+	public String getConvertedValueFromCurrencyAndAmount(String currency, double amount) {
 		return businessServiceObj.getConvertedValueFromCurrencyAndAmount(currency, amount);
 	}
 
